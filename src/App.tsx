b719237d09e7e1296c2f493b11273279c3ef6cae
Victor Castilho <victor.castilho@paynet.net.br>
2025-03-11 11:53:48 -0300

    Route fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-// import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
@@ -14,14 +13,15 @@ function App() {
     <UserProvider>
       <Router>
         <div className="container mt-4">
-            <Routes>
-            <Route path="/login" element={<Login setIsAuthenticated={() => {}} />} />
+          <Routes>
+            <Route path="/" element={<Navigate to="/login" replace />} />
+            <Route path="/login" element={<Login setIsAuthenticated={() => { }} />} />
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/donate" element={<Donate />} />
             <Route path="/history" element={<History />} />
             <Route path="/favorites" element={<Favorites />} />
             <Route path="/register" element={<Register />} />
-            </Routes>
+          </Routes>
         </div>
       </Router>
     </UserProvider>
